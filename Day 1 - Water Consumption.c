1af333eb21d626ab2b406dd2f65c8cbf6413fde8
Nishant Kumar <144876485+GrowlingBear01@users.noreply.github.com>
2023-09-18 18:01:54 +0530

    Add files via upload

---
+++ Day 1 - Water Consumption.c
@@ -0,0 +1,11 @@
+int main(){
+    int X;
+    printf("How much amount of water you drank today- ");
+    scanf("%d",&X);
+    if(X>=2000){
+        printf("YES,You are following advise of doctor Well done");
+    }else{
+        printf("No,You are not following advise of doctor");
+    }
+   
+}
