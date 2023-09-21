621611190f7ec8f77e9ef4f263ca32b2f1906ee0
Nishant Kumar <144876485+GrowlingBear01@users.noreply.github.com>
2023-09-21 20:17:47 +0530

    Add files via upload

---
+++ Day 3 - Sum of Two Numbers.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,sum;
+    printf("Enter two Numbers ");
+    scanf("%d%d",&num1,&num2);
+    sum=num1+num2;
+    printf("Sum=%d",sum);
+
+    return 0;
+}
