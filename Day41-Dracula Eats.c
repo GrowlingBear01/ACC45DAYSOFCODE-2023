6704d9173185ed09327a7b39551136b6c29eb126
Nishant Kumar <144876485+GrowlingBear01@users.noreply.github.com>
2023-10-28 21:02:18 +0530

    Create Day41-Dracula Eats.c

---
+++ Day41-Dracula Eats.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+    scanf("%d",&t);
+    while(t--){
+        int n;
+        scanf("%d",&n);
+        int frequency=n/7;
+        if(n%7>1){
+            frequency++;
+        }
+        printf("%d\n",frequency);
+    }
+	return 0;
+}
